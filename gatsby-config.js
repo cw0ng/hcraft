8c047a6dd37f82564db0a73bbab36f3eed99a379
Corina Wong <hello@corinawong.com>
2019-05-16 12:30:24 -0400

    Setup default layout for mdx pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
     {
+      resolve: "gatsby-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
