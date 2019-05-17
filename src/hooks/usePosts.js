a7da3170e4f82b6f636505e38cdcea3a5e60d681
Corina Wong <hello@corinawong.com>
2019-05-17 13:11:19 -0400

    Filter to only show posts category

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -3,24 +3,29 @@ import { graphql, useStaticQuery } from "gatsby"
 const usePosts = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMdx {
+      allFile(
+        filter: { sourceInstanceName: { eq: "posts" } }
+        sort: { fields: [relativePath], order: DESC }
+      ) {
         nodes {
-          frontmatter {
-            title
-            slug
-            author
+          sourceInstanceName
+          relativePath
+          childMdx {
+            frontmatter {
+              title
+              slug
+            }
           }
-          excerpt
         }
       }
     }
   `)
 
-  return data.allMdx.nodes.map(post => ({
-    title: post.frontmatter.title,
-    author: post.frontmatter.author,
-    slug: post.frontmatter.slug,
-    excerpt: post.excerpt,
+  return data.allFile.nodes.map(post => ({
+    title: post.childMdx.frontmatter.title,
+    author: post.childMdx.frontmatter.author,
+    slug: post.childMdx.frontmatter.slug,
+    excerpt: post.childMdx.excerpt,
   }))
 }
 
